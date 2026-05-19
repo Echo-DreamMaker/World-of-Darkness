53548d2c6632ca531cbbd9b69fe608a494fbc5d9
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-05-19 19:58:07 +0300

    Merge pull request #9 from MataVsn/erp-core
    [Feature] ERP - core systems

--- Content.Client/Clothing/ClientClothingSystem.cs
+++ Content.Client/Clothing/ClientClothingSystem.cs
@@ -344,6 +344,7 @@ public sealed class ClientClothingSystem : ClothingSystem
 
                     break;
                 case Sex.Female:
+                case Sex.Futanari: // Arcane-edit
                     if (inventory.FemaleDisplacements.Count > 0)
                     {
                         if (!string.IsNullOrEmpty(clothingComponent.ClothingType))
